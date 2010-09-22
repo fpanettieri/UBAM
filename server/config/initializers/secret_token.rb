f2f14b10b41d804e1924238e5af10fe143d53e3b
Fabio R. Panettieri <fpanettieri@xoomcode.com>
2010-09-22 12:05:24 -0300

    Added server

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = 'c14487b3a9df1e8c72cdf999581e9d9e7a0a81d351b66de6a1c8e8a4a2f7a2ab649f8a3266ddb1e1125e757de5e7117dd3affcfbd42f77dd967c879b0268af38'
